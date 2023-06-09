8351d0f30ed4e36676776b47ffb741770bfe2e44
Abhishek Bhatt <121381930+TrueCoder2@users.noreply.github.com>
2023-06-09 06:57:05 +0000

    changed image componnt

--- src/Component/Movies.js
+++ src/Component/Movies.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Movies = (Props) => {
   return (
     <div className='movies'>
-   <img src={Props.poster} /> <br/><br/>
+   <img src={Props.poster} alt='movie poster' /> <br/><br/>
    <p> {Props.title} </p><br/>
    <p> Year: {Props.year} </p>
    <br/>
